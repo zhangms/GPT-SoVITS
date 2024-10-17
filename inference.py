222fa0f7a205410670c02e4f0fe4cd2141ffffab
zms <zms>
2024-10-17 14:30:38 +0800

    x

--- inference.py
+++
@@ -1,6 +0,0 @@
-class InferenceModel(object):
-    def __init__(self, speaker_id: str):
-        print("Initializing model")
-
-    def inf(self, text: str, speaker: str, language: str):
-        print("inference with", self)
