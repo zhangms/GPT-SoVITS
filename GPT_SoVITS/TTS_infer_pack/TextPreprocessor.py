06a99bc09a3a2c7c1df272eeb52a1a7390244802
zms <zms>
2024-10-17 16:24:09 +0800

    x

--- GPT_SoVITS/TTS_infer_pack/TextPreprocessor.py
+++ GPT_SoVITS/TTS_infer_pack/TextPreprocessor.py
@@ -1,7 +1,6 @@
+import os
+import sys
 
-import os, sys
-
-from tqdm import tqdm
 now_dir = os.getcwd()
 sys.path.append(now_dir)
 
@@ -17,17 +16,19 @@ from TTS_infer_pack.text_segmentation_method import split_big_text, splits, get_
 
 from tools.i18n.i18n import I18nAuto, scan_language_list
 
-language=os.environ.get("language","Auto")
-language=sys.argv[-1] if sys.argv[-1] in scan_language_list() else language
+language = os.environ.get("language", "Auto")
+language = sys.argv[-1] if sys.argv[-1] in scan_language_list() else language
 i18n = I18nAuto(language=language)
-punctuation = set(['!', '?', '…', ',', '.', '-'," "])
+punctuation = set(['!', '?', '…', ',', '.', '-', " "])
 
-def get_first(text:str) -> str:
+
+def get_first(text: str) -> str:
     pattern = "[" + "".join(re.escape(sep) for sep in splits) + "]"
     text = re.split(pattern, text)[0].strip()
     return text
 
-def merge_short_text_in_array(texts:str, threshold:int) -> list:
+
+def merge_short_text_in_array(texts: str, threshold: int) -> list:
     if (len(texts)) < 2:
         return texts
     result = []
@@ -45,27 +46,24 @@ def merge_short_text_in_array(texts:str, threshold:int) -> list:
     return result
 
 
-
-
-
 class TextPreprocessor:
-    def __init__(self, bert_model:AutoModelForMaskedLM, 
-                 tokenizer:AutoTokenizer, device:torch.device):
+    def __init__(self, bert_model: AutoModelForMaskedLM,
+                 tokenizer: AutoTokenizer, device: torch.device):
         self.bert_model = bert_model
         self.tokenizer = tokenizer
         self.device = device
-        
-    def preprocess(self, text:str, lang:str, text_split_method:str, version:str="v2")->List[Dict]:
+
+    def preprocess(self, text: str, lang: str, text_split_method: str, version: str = "v2") -> List[Dict]:
         print(i18n("############ 切分文本 ############"))
         text = self.replace_consecutive_punctuation(text)
         texts = self.pre_seg_text(text, lang, text_split_method)
         result = []
         print(i18n("############ 提取文本Bert特征 ############"))
-        for text in tqdm(texts):
+        for text in texts:
             phones, bert_features, norm_text = self.segment_and_extract_feature_for_text(text, lang, version)
-            if phones is None or norm_text=="":
+            if phones is None or norm_text == "":
                 continue
-            res={
+            res = {
                 "phones": phones,
                 "bert_features": bert_features,
                 "norm_text": norm_text,
@@ -73,18 +71,18 @@ class TextPreprocessor:
             result.append(res)
         return result
 
-    def pre_seg_text(self, text:str, lang:str, text_split_method:str):
+    def pre_seg_text(self, text: str, lang: str, text_split_method: str):
         text = text.strip("\n")
         if len(text) == 0:
             return []
-        if (text[0] not in splits and len(get_first(text)) < 4): 
+        if (text[0] not in splits and len(get_first(text)) < 4):
             text = "。" + text if lang != "en" else "." + text
         print(i18n("实际输入的目标文本:"))
         print(text)
-        
+
         seg_method = get_seg_method(text_split_method)
         text = seg_method(text)
-        
+
         while "\n\n" in text:
             text = text.replace("\n\n", "\n")
 
@@ -93,32 +91,32 @@ class TextPreprocessor:
         _texts = merge_short_text_in_array(_texts, 5)
         texts = []
 
-        
         for text in _texts:
             # 解决输入目标文本的空行导致报错的问题
             if (len(text.strip()) == 0):
-               continue
-            if not re.sub("\W+", "", text):       
+                continue
+            if not re.sub("\W+", "", text):
                 # 检测一下，如果是纯符号，就跳过。
                 continue
             if (text[-1] not in splits): text += "。" if lang != "en" else "."
-            
+
             # 解决句子过长导致Bert报错的问题
             if (len(text) > 510):
                 texts.extend(split_big_text(text))
             else:
                 texts.append(text)
-            
+
         print(i18n("实际输入的目标文本(切句后):"))
         print(texts)
         return texts
-    
-    def segment_and_extract_feature_for_text(self, text:str, language:str, version:str="v1")->Tuple[list, torch.Tensor, str]:
+
+    def segment_and_extract_feature_for_text(self, text: str, language: str, version: str = "v1") -> Tuple[
+        list, torch.Tensor, str]:
         return self.get_phones_and_bert(text, language, version)
-        
-    def get_phones_and_bert(self, text:str, language:str, version:str, final:bool=False):
+
+    def get_phones_and_bert(self, text: str, language: str, version: str, final: bool = False):
         if language in {"en", "all_zh", "all_ja", "all_ko", "all_yue"}:
-            language = language.replace("all_","")
+            language = language.replace("all_", "")
             if language == "en":
                 LangSegment.setfilters(["en"])
                 formattext = " ".join(tmp["text"] for tmp in LangSegment.getTexts(text))
@@ -131,14 +129,14 @@ class TextPreprocessor:
                 if re.search(r'[A-Za-z]', formattext):
                     formattext = re.sub(r'[a-z]', lambda x: x.group(0).upper(), formattext)
                     formattext = chinese.mix_text_normalize(formattext)
-                    return self.get_phones_and_bert(formattext,"zh",version)
+                    return self.get_phones_and_bert(formattext, "zh", version)
                 else:
                     phones, word2ph, norm_text = self.clean_text_inf(formattext, language, version)
                     bert = self.get_bert_feature(norm_text, word2ph).to(self.device)
             elif language == "yue" and re.search(r'[A-Za-z]', formattext):
-                    formattext = re.sub(r'[a-z]', lambda x: x.group(0).upper(), formattext)
-                    formattext = chinese.mix_text_normalize(formattext)
-                    return self.get_phones_and_bert(formattext,"yue",version)
+                formattext = re.sub(r'[a-z]', lambda x: x.group(0).upper(), formattext)
+                formattext = chinese.mix_text_normalize(formattext)
+                return self.get_phones_and_bert(formattext, "yue", version)
             else:
                 phones, word2ph, norm_text = self.clean_text_inf(formattext, language, version)
                 bert = torch.zeros(
@@ -146,9 +144,9 @@ class TextPreprocessor:
                     dtype=torch.float32,
                 ).to(self.device)
         elif language in {"zh", "ja", "ko", "yue", "auto", "auto_yue"}:
-            textlist=[]
-            langlist=[]
-            LangSegment.setfilters(["zh","ja","en","ko"])
+            textlist = []
+            langlist = []
+            LangSegment.setfilters(["zh", "ja", "en", "ko"])
             if language == "auto":
                 for tmp in LangSegment.getTexts(text):
                     langlist.append(tmp["lang"])
@@ -184,12 +182,11 @@ class TextPreprocessor:
             norm_text = ''.join(norm_text_list)
 
         if not final and len(phones) < 6:
-            return self.get_phones_and_bert("." + text,language,version,final=True)
+            return self.get_phones_and_bert("." + text, language, version, final=True)
 
         return phones, bert, norm_text
 
-
-    def get_bert_feature(self, text:str, word2ph:list)->torch.Tensor:
+    def get_bert_feature(self, text: str, word2ph: list) -> torch.Tensor:
         with torch.no_grad():
             inputs = self.tokenizer(text, return_tensors="pt")
             for i in inputs:
@@ -203,14 +200,14 @@ class TextPreprocessor:
             phone_level_feature.append(repeat_feature)
         phone_level_feature = torch.cat(phone_level_feature, dim=0)
         return phone_level_feature.T
-    
-    def clean_text_inf(self, text:str, language:str, version:str="v2"):
+
+    def clean_text_inf(self, text: str, language: str, version: str = "v2"):
         phones, word2ph, norm_text = clean_text(text, language, version)
         phones = cleaned_text_to_sequence(phones, version)
         return phones, word2ph, norm_text
 
-    def get_bert_inf(self, phones:list, word2ph:list, norm_text:str, language:str):
-        language=language.replace("all_","")
+    def get_bert_inf(self, phones: list, word2ph: list, norm_text: str, language: str):
+        language = language.replace("all_", "")
         if language == "zh":
             feature = self.get_bert_feature(norm_text, word2ph).to(self.device)
         else:
@@ -221,24 +218,19 @@ class TextPreprocessor:
 
         return feature
 
-
-    def filter_text(self,texts):
-        _text=[]
-        if all(text in [None, " ", "\n",""] for text in texts):
+    def filter_text(self, texts):
+        _text = []
+        if all(text in [None, " ", "\n", ""] for text in texts):
             raise ValueError(i18n("请输入有效文本"))
         for text in texts:
-            if text in  [None, " ", ""]:
+            if text in [None, " ", ""]:
                 pass
             else:
                 _text.append(text)
         return _text
-    
 
-    def replace_consecutive_punctuation(self,text):
+    def replace_consecutive_punctuation(self, text):
         punctuations = ''.join(re.escape(p) for p in punctuation)
         pattern = f'([{punctuations}])([{punctuations}])+'
         result = re.sub(pattern, r'\1', text)
         return result
-
-
-
