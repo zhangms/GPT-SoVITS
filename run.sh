ea668e639444de527f095677123b0adf32cc1cb6
zms <zms>
2024-10-18 11:31:39 +0800

    x

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-nohup python -m uvicorn api_v3:APP --host 0.0.0.0 --port 7080 --workers 4 >> /workspace/res/out.txt 2>&1
+nohup python -m uvicorn api_v3:APP --host 0.0.0.0 --port 7080 --workers 4 >> /workspace/res/out.txt 2>&1 &
