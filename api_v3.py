92906cdf26afa912f438c60a9794462f4bc93174
zms <zms>
2024-10-29 18:18:55 +0800

    flush

--- api_v3.py
+++ api_v3.py
@@ -29,41 +29,38 @@ class TTSRequest(BaseModel):
     text: str = None
     speaker_id: str = ""
     trace_id: str = ""
+    format: str = "mp3"
 
 
-def pack_mp3(data: np.ndarray, rate: int):
+def pack_voice(data: np.ndarray, rate: int, fmt: str):
     io_buffer = BytesIO()
-    sf.write(io_buffer, data, rate, format='mp3')
+    if fmt.lower() == "wav":
+        sf.write(io_buffer, data, rate, format='wav')
+    elif fmt.lower() == "mp3":
+        sf.write(io_buffer, data, rate, format='mp3')
+    else:
+        sf.write(io_buffer, data, rate, format='wav')
+
     io_buffer.seek(0)
     return io_buffer
 
 
-def pack_mp3_b64(data: np.ndarray, rate: int):
-    mp3_data = pack_mp3(data, rate).getvalue()
-    encoded_content = base64.b64encode(mp3_data)
+def pack_voice_b64(data: np.ndarray, rate: int, fmt: str):
+    voice_data = pack_voice(data, rate, fmt).getvalue()
+    encoded_content = base64.b64encode(voice_data)
     return b'data:' + encoded_content + b'\n'
 
 
-async def tts_handle(trace_id, speaker_id, text):
+async def tts_handle(trace_id, speaker_id, text, fmt):
     try:
         sr, audio = tts_pipline.inference(trace_id, speaker_id, text)
-        audio_data = pack_mp3(audio, sr).getvalue()
+        audio_data = pack_voice(audio, sr, fmt).getvalue()
         return Response(audio_data, media_type=f"audio/mp3")
     except Exception as ex:
         return JSONResponse(status_code=500, content=f"TTS_SERVICE_ERROR:{ex}")
 
 
-async def tts_base64_handle(trace_id, speaker_id, text):
-    try:
-        sr, audio = tts_pipline.inference(trace_id, speaker_id, text)
-        audio_data = pack_mp3(audio, sr).getvalue()
-        encoded_content = base64.b64encode(audio_data)
-        return {"audio": encoded_content}
-    except Exception as ex:
-        return JSONResponse(status_code=500, content=f"TTS_SERVICE_ERROR:{ex}")
-
-
-async def tts_stream_handle(trace_id, speaker_id, text):
+async def tts_stream_handle(trace_id, speaker_id, text, fmt):
     try:
         tts_gen = tts_pipline.generator(speaker_id, text)
 
@@ -75,7 +72,7 @@ async def tts_stream_handle(trace_id, speaker_id, text):
                 end = time.time()
                 rt = end - start
                 print(f"{datetime.datetime.now()}|TTS_STREAM_GENERATOR|{trace_id}|{speaker_id}|index:{index}|rt:{rt}")
-                yield pack_mp3_b64(chunk, sr)
+                yield pack_voice_b64(chunk, sr, fmt)
             print("", flush=True)
 
         return StreamingResponse(streaming_generator(tts_gen), media_type="text/event-stream")
@@ -89,19 +86,14 @@ async def check_health():
 
 
 @APP.get("/tts")
-async def tts(trace_id: str, text: str, speaker_id: str):
-    return await tts_handle(trace_id, speaker_id, text)
-
-
-@APP.post("/audgeneratebase64")
-async def audgeneratebase64(request: TTSRequest):
-    return await tts_base64_handle(request.trace_id, request.speaker_id, request.text)
+async def tts(trace_id: str, speaker_id: str, text: str, fmt: str):
+    return await tts_handle(trace_id, speaker_id, text, fmt)
 
 
 @APP.get("/tts_stream")
-async def tts_stream(trace_id: str, text: str, speaker_id: str):
-    print(f"{datetime.datetime.now()}|TTS_STREAM|{trace_id}|{speaker_id}|{text}")
-    return await tts_stream_handle(trace_id, speaker_id, text)
+async def tts_stream(trace_id: str, speaker_id: str, text: str, fmt: str):
+    print(f"{datetime.datetime.now()}|TTS_STREAM|{trace_id}|{speaker_id}|{fmt}|{text}")
+    return await tts_stream_handle(trace_id, speaker_id, text, fmt)
 
 
 if __name__ == "__main__":
